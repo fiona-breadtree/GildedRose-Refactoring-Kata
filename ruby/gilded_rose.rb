2d338cb173a9595eb947f535e30b3f6e93dc7de9
fiona.man <fiona.man@rea-group.com>
2016-08-26 10:16:33 +1000

    Fix some error

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,29 +6,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      case item.name
-      when '+5 Dexterity Vest' then
-        item.sell_in = item.sell_in - 1
-        item.quality = item.quality - 1
-      when 'Aged Brie' then
-        item.sell_in = item.sell_in - 1
-        if item.quality != 0
-          item.quality = item.quality - 1
-        else
-          item.quality = 1
-        end
-      when 'Elixir of the Mongoose' then
-        item.sell_in = item.sell_in - 1
-        item.quality = item.quality - 1
-      when 'Sulfuras, Hand of Ragnaros'
-        # do nothing
-      when 'Backstage passes to a TAFKAL80ETC concert' then
-        item.sell_in = item.sell_in - 1
-        item.quality = item.quality + 1
-      when 'Conjured Mana Cake' then
-        item.sell_in = item.sell_in - 1
-        item.quality = item.quality - 1
-      end
+      item.update_quality
     end
   end
 end
@@ -42,7 +20,71 @@ class Item
     @quality = quality
   end
 
+  def update_quality
+    case name
+    when '+5 Dexterity Vest' then
+      @sell_in = sell_in - 1
+      @quality = quality - 1
+    when 'Aged Brie' then
+      @sell_in = sell_in - 1
+      if quality != 0
+        @quality = quality - 1
+      else
+        @quality = 1
+      end
+    when 'Elixir of the Mongoose' then
+      @sell_in = sell_in - 1
+      @quality = quality - 1
+    when 'Sulfuras, Hand of Ragnaros'
+      # do nothing
+    when 'Backstage passes to a TAFKAL80ETC concert' then
+      @sell_in = sell_in - 1
+      @quality = quality + 1
+    when 'Conjured Mana Cake' then
+      @sell_in = sell_in - 1
+      @quality = quality - 1
+    end
+  end
+
   def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+    "#{name}, #{sell_in}, #{quality}"
+  end
+end
+
+class DexterityVestItem < Item
+  def update_quality
+    sell_in = sell_in - 1
+    quality = quality - 1
+  end
+end
+
+class AgedBrieItem < Item
+  def update_quality
+    sell_in = sell_in - 1
+    if quality != 0
+      quality = quality - 1
+    else
+      quality = 1
+    end
+  end
+end
+
+class ElixirOfTheMongooseItem < Item
+  def update_quality
+    sell_in = sell_in - 1
+    quality = quality - 1
+  end
+end
+
+class SulfurasHandofRagnarosItem < Item
+  def update_quality
+    # do nothing
+  end
+end
+
+class BackstagePassesItem < Item
+  def update_quality
+    sell_in = sell_in - 1
+    quality = quality + 1
   end
 end
